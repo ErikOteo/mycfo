cf804428dd7dcf897e9a1bea364d8ddce1ee96cb
Erik <49723973+ErikOteo@users.noreply.github.com>
2026-01-06 14:17:08 -0300

    Modify component scanning and remove scheduling
    Removed scheduling support and added component scanning for specific packages.

--- pronostico/src/main/java/pronostico/PronosticoApplication.java
+++ pronostico/src/main/java/pronostico/PronosticoApplication.java
@@ -2,10 +2,11 @@ package pronostico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableScheduling
+// FORZAMOS A SPRING A BUSCAR EN EL PAQUETE CONFIG
+@ComponentScan(basePackages = {"pronostico", "pronostico.config", "pronostico.controllers", "pronostico.services"})
 public class PronosticoApplication {
 
 	public static void main(String[] args) {
